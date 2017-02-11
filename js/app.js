5fc5eba27d6f86f0cc2de5778ad78d4f42ad6c53
Ash <ash.duckett@outlook.com>
2017-02-11 18:35:56 +0000

    Markers are all now visible on the map.

--- js/app.js
+++ js/app.js
@@ -9,10 +9,10 @@ function initMap() {
 		{title: '178 Vallance Road', location: {lat: 51.524026, lng: -0.06396}},
 		{title: 'The Blind Beggar', location: {lat: 51.520052, lng: -0.056939}},
 		{title: 'Pellicci\'s Café', location: {lat: 51.52649, lng: -0.063428}},
-		{title: 'Esmarelda\'s Barn', location: {lat: 51.501943, lng: -0156234}},
-		{title: 'Bow Majistrates Court', location: {lat: 51.513606, lng: -0122323}},
+		{title: 'Esmarelda\'s Barn', location: {lat: 51.501943, lng: -0.156234}},
+		{title: 'Bow Majistrates Court', location: {lat: 51.527344, lng: -0.023625}},
 		{title: 'The Tower of London', location: {lat: 51.508112, lng: -0.075949}},
-		{title: 'The Royal Oak', location: {lat: 42.490602, lng: -83.174562}},
+		{title: 'The Royal Oak', location: {lat: 51.529547, lng: -0.069298}},
 		{title: 'Repton Boy\'s Club', location: {lat: 51.524215, lng: -0.065311}},
 		{title: 'The Carpenter\'s Arms', location: {lat: 51.523962, lng: -0.067356}},
 		{title: 'The Speakeasy Club', location: {lat: 51.516436, lng: -0.141724}},
@@ -22,5 +22,45 @@ function initMap() {
 		{title: 'Wiliton Music Hall', location: {lat: 51.510701, lng: -0.066897}},
 		{title: 'The Tower of London', location: {lat: 51.525074, lng: -0.06693}},
 	];
+
+
+	var largeInfoWindow = new google.maps.InfoWindow();
+	var bounds = new google.maps.LatLngBounds();
+
+	for(var i = 0; i < locations.length; i++) {
+		var position = locations[i].location;
+		var title = locations[i].title;
+
+		var marker = new google.maps.Marker({
+			map: map,
+			position: position,
+			title: title,
+			animation: google.maps.Animation.DROP,
+			id: i
+		});
+
+		bounds.extend(marker.position);
+
+		//markers.push(marker);
+
+		marker.addListener('click', function() {
+			populateInfoWindow(this, largeInfoWindow);
+		});
+
+		map.fitBounds(bounds);
+
+		function populateInfoWindow(marker, infoWindow) {
+			// Check to make sure the infoWindow is not already opened on this marker
+			if(infoWindow.marker != marker) {
+				infoWindow.marker = marker;
+				infoWindow.setContent('<div>' + marker.title + '</div>');
+				infoWindow.open(map, marker);
+
+				infoWindow.addListener('closeClick', function() {
+					infoWindow.setMarker(null);
+				});
+			}
+		}
+	}
 
 }
